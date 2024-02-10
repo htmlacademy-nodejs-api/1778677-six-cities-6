443c263ae5e814ce485e0824c59d9aeee7fa4571
Alina Mangusheva <alinamr@mail.ru>
2024-02-10 22:13:42 +0300

    feat: добавлено определение модели и схемы

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true, minlength: [1,'Min length for name is 1'], maxlength: [15,'Max length for name is 15'] })
   public name: string;
+
+  @prop({ required: false, unique: true, match: [ /^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect']})
   public email: string;
+
+  @prop({ required: false, match: [/\.(jpg|png)$/i, 'User image not in .jpg or .png format']})
   public avatar: string;
+
+  @prop({ required: true, minlength: [6,'Min length for password is 6'], maxlength: [12,'Max length for password is 12']})
   public password: string;
+
+  @prop({ required: true, enum: Object.values(UserType)})
   public userType: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
