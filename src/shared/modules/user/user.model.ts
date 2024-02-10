6bc8baf0ac40b362cac5de9f1bab6272bb024b02
Alina Mangusheva <alinamr@mail.ru>
2024-02-10 21:51:04 +0300

    feat: задействована встроенная валидация

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -8,13 +8,27 @@ export interface UserDocument extends User, Document {
 }
 
 const userSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: [1,'Min length for name is 1'],
+    maxlength: [15,'Max length for name is 15'],
+  },
   email: {
     type: String,
     unique: true,
+    match: [ /^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
   },
-  avatar: String,
-  password: String,
+  avatar: {
+    type: String,
+    match: [/\.(jpg|png)$/i, 'User image not in .jpg or .png format'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6,'Min length for password is 6'],
+    maxlength: [12,'Max length for password is 12'],
+  },
   userType: {
     type: String,
     enum: Object.values(UserType),
