3b9840f1bacd0ae1ac29df7ee483ba29364b9059
Alina Mangusheva <alinamr@mail.ru>
2024-02-10 22:57:58 +0300

    feat: добавлены методы в UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,8 +1,16 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
@@ -15,11 +23,28 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, match: [/\.(jpg|png)$/i, 'User image not in .jpg or .png format']})
   public avatar: string;
 
-  @prop({ required: true, minlength: [6,'Min length for password is 6'], maxlength: [12,'Max length for password is 12']})
-  public password: string;
+  @prop({ required: true, minlength: [6,'Min length for password is 6'], maxlength: [12,'Max length for password is 12'], default: ''})
+  private password?: string;
 
   @prop({ required: true, enum: Object.values(UserType)})
   public userType: UserType;
+
+  constructor(userData: User) {
+    super();
+
+    this.name = userData.name;
+    this.email = userData.email;
+    this.avatar = userData.avatar;
+    this.userType = userData.userType;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
